81d8a0b8640735d2a5850d8f6edfa63ac998e692
Ignacio Veiga <ignacioveigagiusti@gmail.com>
2022-09-24 08:25:46 -0300

    isvserver with improved checks and websocket for chat

--- isvserver.js
+++ isvserver.js
@@ -1,25 +1,117 @@
 const express = require('express');
 const { Router } = express;
-
-// IMPORTANT! body-parser and method-override must be installed with npm for everything to run correctly
-const methodOverride = require('method-override')
-const bodyParser = require('body-parser');
+const fs = require('fs');
+const { Server: IOServer } = require('socket.io');
+const { Server: HttpServer } = require('http');
 
 const app = express();
+const httpServer = new HttpServer(app);
+const io = new IOServer(httpServer);
+
 const productRouter = new Router();
 
 const Products = require('./api/products.js');
 const productContainer = new Products('./api/products.json');
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 productRouter.use(express.json());
-productRouter.use(express.urlencoded({ extended: true }));
-productRouter.use(bodyParser.urlencoded({ extended: false }));
-app.use(methodOverride('_method'))
-productRouter.use(methodOverride('_method'))
+productRouter.use(express.urlencoded({ extended: false }));
 
-// public/index.html is sent when performing a get on the root directory
-app.get('/', (req, res) => {
-    res.sendFile('./public/index.html', {root:__dirname});
+// Pug settings for templating
+app.set('view engine', 'pug');
+app.set('views', './views');
+app.use(express.static('public'));
+
+// index.html is sent when performing a get on the root directory
+app.get('/', async (req, res) => {
+    let allProducts = []
+    try {
+        allProducts = await productContainer.getAll();
+    } catch (err) {
+        res.render('pages/index.pug', {error: err});
+    }
+    res.render('pages/index.pug');
+})
+
+app.post('/', async (req, res) => {
+    try {
+        if( req.body.title == undefined || req.body.price === null || req.body.thumbnail == undefined || req.body.category == undefined || req.body.stock == null || req.body.title == '' || req.body.price === '' || req.body.thumbnail == '' || req.body.category == '' || req.body.stock == '' ) {
+            throw 'Missing data. Product needs Title, Price, Thumbnail, Category and Stock.'
+        }
+        let category = req.body.category;
+        let subcategory = req.body.subcategory;
+        let title = req.body.title;
+        let description = req.body.description;
+        let price = req.body.price;
+        let stock = req.body.stock;
+        let thumbnail = req.body.thumbnail;
+        price = parseFloat(price);
+        stock = parseFloat(stock);
+        const newProduct = {category:category, subcategory:subcategory, title:title, description:description, price:price, stock:stock, thumbnail:thumbnail};
+        const savedProduct = await productContainer.save(newProduct);
+        res.send(JSON.stringify(savedProduct));
+    } catch (err) {
+        res.status(400).send(err);
+    }
+})
+
+app.post('/edit', async (req, res) => {
+    try {
+        let putId;
+        if (req.body.id != null && req.body.id !== '') {
+            putId = req.body.id;
+        }
+        else{
+            throw 'No ID was provided';
+        }
+        const prevProduct = await productContainer.getById(putId);
+        let newCategory = prevProduct.category;
+        let newSubcategory = prevProduct.subcategory;
+        let newTitle = prevProduct.title;
+        let newDescription = prevProduct.description;
+        let newPrice = prevProduct.price;
+        let newStock = prevProduct.stock;
+        let newThumbnail = prevProduct.thumbnail;
+        if (typeof req.body.category === 'string' && req.body.category !== '') {
+            newCategory = req.body.category;
+        }
+        if (typeof req.body.subcategory === 'string' && req.body.subcategory !== '') {
+            newSubcategory = req.body.subcategory;
+        }
+        if (typeof req.body.title === 'string' && req.body.title !== '') {
+            newTitle = req.body.title;
+        }
+        if (typeof req.body.description === 'string' && req.body.description !== '') {
+            newDescription = req.body.description;
+        }
+        if (!isNaN(req.body.price) && req.body.price && req.body.price !== '') {
+            newPrice = parseFloat(req.body.price);
+        }
+        if (!isNaN(req.body.stock) && req.body.stock && req.body.stock !== '') {
+            newStock = parseFloat(req.body.stock);
+        }      
+        if (typeof req.body.thumbnail === 'string' && req.body.thumbnail !== '') {   
+            newThumbnail = req.body.thumbnail;
+        }
+        const newProduct = {category:newCategory, subcategory:newSubcategory, title:newTitle, description:newDescription, price:newPrice, stock:newStock, thumbnail:newThumbnail};
+        const editProduct = await productContainer.edit(putId, newProduct).catch((err) => {
+            throw err
+        });
+        res.send(editProduct);
+    } catch (err) {
+        res.status(400).send(err.message || err);
+    }
+});
+
+app.get('/products', async (req, res) => {
+    let allProducts = []
+    try {
+        allProducts = await productContainer.getAll();
+    } catch (err) {
+        res.render('pages/productView.pug', {error: err});
+    }
+    res.render('pages/productView.pug', {productList: allProducts});
 })
 
 // get all products from /api/products
@@ -46,59 +138,60 @@ productRouter.get('/:id', async (req, res) => {
 // add one product with a post method to /api/products
 productRouter.post('/', async (req, res) => {
     try {
-        if( req.body.title == undefined || isNaN(req.body.price) || req.body.thumbnail == undefined || req.body.title == '' || req.body.price == '' || req.body.thumbnail == '' ) {
-            throw 'Missing data. Product needs Title, Price and Thumbnail.'
+        if( req.body.title == undefined || req.body.price === null || req.body.thumbnail == undefined || req.body.category == undefined || req.body.stock == null || req.body.title == '' || req.body.price === '' || req.body.thumbnail == '' || req.body.category == '' || req.body.stock == '' ) {
+            throw 'Missing data. Product needs Title, Price, Thumbnail, Category and Stock.'
         }
-        if( req.body.price < 0) {
-            throw 'Price must be equal to or greater than zero.'
-        }
+        let category = req.body.category;
+        let subcategory = req.body.subcategory || ' ';
         let title = req.body.title;
+        let description = req.body.description || ' ';
         let price = req.body.price;
+        let stock = req.body.stock;
         let thumbnail = req.body.thumbnail;
         price = parseFloat(price);
-        const newProduct = {title:title, price:price, thumbnail:thumbnail};
+        stock = parseFloat(stock);
+        const newProduct = {category:category, subcategory:subcategory, title:title, description:description, price:price, stock:stock, thumbnail:thumbnail};
         const savedProduct = await productContainer.save(newProduct);
-        res.send(`Product added succesfully: ${JSON.stringify(savedProduct)}`);
+        res.send(`Producto añadido: ${JSON.stringify(savedProduct)}`);
     } catch (err) {
         res.send(`${err}`);
     }
 });
 
-// takes a post method from the html form to edit files and uses method override to turn it into a put method with the product id 
-// this method cannot be tested with postman or a direct put method, it only works when using a form in an html and then posting it with the PUT method in the URL
-productRouter.put('/', (req,res) => {
-    try {
-        let putId = req.body.id;
-        res.redirect(307, `products/${putId}?_method=PUT`)
-    } catch (err) {
-        res.send(`${err}`);
-    }
-})
-
 // PUT method to edit a product by ID (this is the one that can be tested with postman)
 productRouter.put('/:id', async (req, res) => {
     try {
-        // if( req.body.title == undefined || isNaN(req.body.price) || req.body.thumbnail == undefined || req.body.title == '' || req.body.price == '' ||  req.body.thumbnail == '' ) {
-        //     throw 'Missing data. Product needs Title, Price and Thumbnail.'
-        // }
-        if(req.body.price && (req.body.price < 0 || isNaN(req.body.price))) {
-            throw 'Price must be equal to or greater than zero.'
-        }
         const param = req.params.id;
-        const prevProduct = await productContainer.getById(param);
+        const prevProduct = productContainer.getById(param);
+        let newCategory = prevProduct.category;
+        let newSubcategory = prevProduct.subcategory || '';
         let newTitle = prevProduct.title;
+        let newDescription = prevProduct.description || '';
         let newPrice = prevProduct.price;
+        let newStock = prevProduct.stock;
         let newThumbnail = prevProduct.thumbnail;
+        if (typeof req.body.category === 'string' && req.body.category !== '') {
+            newCategory = req.body.category;
+        }
+        if (typeof req.body.subcategory === 'string' && req.body.subcategory !== '') {
+            newSubcategory = req.body.subcategory;
+        }
         if (typeof req.body.title === 'string' && req.body.title !== '') {
             newTitle = req.body.title;
         }
-        if (req.body.price && req.body.price >= 0  ) {
+        if (typeof req.body.description === 'string' && req.body.description !== '') {
+            newDescription = req.body.description;
+        }
+        if (parseFloat(req.body.price) != null && req.body.price !== '') {
             newPrice = parseFloat(req.body.price);
-        }    
+        }
+        if (parseFloat(req.body.stock) != null && req.body.stock !== '') {
+            newStock = parseFloat(req.body.stock);
+        }      
         if (typeof req.body.thumbnail === 'string' && req.body.thumbnail !== '') {   
             newThumbnail = req.body.thumbnail;
         }
-        const newProduct = {title:newTitle, price:newPrice, thumbnail:newThumbnail};
+        const newProduct = {category:newCategory, subcategory:newSubcategory, title:newTitle, description:newDescription, price:newPrice, stock:newStock, thumbnail:newThumbnail};
         await productContainer.edit(param, newProduct);
         res.json({id:param, ...newProduct});
     } catch (err) {
@@ -111,7 +204,7 @@ productRouter.delete('/:id', async (req, res) => {
     try {
         const param = req.params.id;
         await productContainer.deleteById(param);
-        res.send(`product with id: ${param} deleted succcessfully`);
+        res.send(`producto con id: ${param} eliminado exitosamente`);
     } catch (err) {
         res.send(`${err}`);
     }
@@ -122,7 +215,41 @@ app.use('/api/products', productRouter);
 
 //Connection
 const PORT = 8080;
-const server = app.listen(PORT, () => {
-    console.log(`Initialized server on port ${server.address().port}`)
+httpServer.listen(PORT, () => {
+    console.log(`Servidor inicializado en el puerto ${httpServer.address().port}`)
 });
-server.on("error", err => console.log(`Server error: ${err}`));
+httpServer.on("error", err => console.log(`Error en el servidor: ${err}`));
+
+io.on('connection', async (socket) => {
+    console.log('Client connected');
+    const messages = JSON.parse(await fs.promises.readFile('./api/messages.json', 'utf8'));
+    try {
+        socket.emit('messages', messages);
+    } catch (err) {
+        io.sockets.emit('msgError', err.message);
+    }
+    let products;
+    try {
+        products = await productContainer.getAll();
+        socket.emit('products', products);
+    } catch (err) {
+        io.sockets.emit('prodError', err.message);
+    }
+    socket.on('newMessage', async data => {
+        try {
+            messages.push(data);
+            await fs.promises.writeFile('./api/messages.json', JSON.stringify(messages,null,2));
+            io.sockets.emit('messages', messages);    
+        } catch (err) {
+            io.sockets.emit('msgError', err.message);
+        }
+    });
+    socket.on('productEvent', async () => {
+        try {
+            products = await productContainer.getAll();
+            io.sockets.emit('products', products);
+        } catch (err) {
+            io.sockets.emit('prodError', err.message);
+        }
+    });
+})
