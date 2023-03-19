a846418abcebd6a98c05a8ede8776b3fe4c6c741
Ignacio Veiga <ignacioveigagiusti@gmail.com>
2023-03-18 23:58:41 -0300

    Desafío - Servidor con Express V1.0

--- server/src/api/daos/products/productFirebaseDao.ts
+++
@@ -1,9 +0,0 @@
-import FirebaseContainer from '../../containers/firebaseContainer';
-
-class ProductFirebaseDao extends FirebaseContainer{
-    constructor(){
-        super(JSON.parse(process.env.FIREBASE_SA_KEY? process.env.FIREBASE_SA_KEY : '{}'), 'products');
-    }
-}
-
-export default ProductFirebaseDao;
